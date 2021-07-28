aeec42fd2dea3ee7169d19575d003d5491d6df81
Nathália Pereira <nathalia.pereira252000@gmail.com>
2021-07-27 23:17:39 -0300

    Valida as respostas dos usuários e conta o score

--- src/pages/Flags/index.js
+++ src/pages/Flags/index.js
@@ -1,10 +1,54 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useParams, useHistory } from 'react-router-dom';
+import { countries } from 'country-data';
 
 const Flags = () => {
+    const history = useHistory();
+    //Armazena o código do país em id
     let { id } = useParams();
+    //Define um país aleatório
+    var randomCountry = require('random-country');
+    var country =  randomCountry();
+
+    //Usa um state para armazenar o score do usuário e o número de questões antes de o jogo terminar
+    const[score, setScore] = useState(0);
+    const[questions, setQuestions] = useState(5);
+
+    //Armazena a resposta do usuário digitada no input
+    const [answer, setAnswer] = useState("");
+
+    //Valida a reposta submetida
+    const validateAnswer = (event) => {
+        //Se a resposta for vazia, não faz nada
+        if (!answer) return;
+        //Previne a página de recarregar
+        event.preventDefault();
+        //Diminui o número de questões
+        setQuestions(questions-1);
+        //Aumenta o score se a resposta for correta
+        if (answer == countries[`${id}`].name) setScore(score+1);
+        //Reseta o input de resposta
+        setAnswer("");
+    }
+    
     return(
-        <h1>Hi</h1>
+        <div>
+            <img src={`https://www.countryflags.io/${id}/flat/64.png`}/>
+            <form id='myInput'>
+                <input
+                    type='text'
+                    placeholder='Que país é esse?'
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}>
+                </input>
+                <button onClick={(event) => validateAnswer(event)}>
+                    {questions == 0 ?
+                        <button onClick={history.push("/")}>Finish game</button>
+                        :<Link to={{pathname: `${!answer ? id : country}`}}>Next</Link>
+                    }
+                </button>
+            </form>
+        </div>
     );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+    //Define um país inicial aleatório
     var randomCountry = require('random-country');
     var country =  randomCountry();
+
     return(
         <button><Link to={{pathname: `flags/${country}`}}>Start</Link></button>
     );
