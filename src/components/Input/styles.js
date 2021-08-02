be4e75532bba0559bdebabc7122b3b969e3a5c00
Nathália Pereira <nathalia.pereira252000@gmail.com>
2021-08-02 15:54:33 -0300

    style: Estiliza input

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -7,12 +7,17 @@ export const Input= styled.input`
     transition: border-color 0.3s ease-in-out;
     outline: 0;
   }
+  ::-webkit-input-placeholder {
+    color: lightpink;
+    opacity: 0.5;
+  }
+  color: lightpink;
   border-radius: 5px;
   padding: 0.5rem 0.5rem;
   margin: 0rem 0rem;
   font-family: "Comic Sans MS", "Comic Sans", cursive;
   background: rgba(0,0,0,.5);
-  opacity: 2;
+
   font-size: 20px;
   text-align: center;
 `
